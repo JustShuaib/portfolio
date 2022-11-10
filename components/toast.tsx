5cad0289f951c71634be6f4e2d640bde46fdd3c0
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-11-10 21:58:18 +0100

    Added emailjs

---
+++ components/toast.tsx
@@ -0,0 +1,11 @@
+const Toast = () => {
+  return (
+    <div>
+      <div className="fixed bottom-0 right-0 m-8">
+        <div className="pointer-events-auto overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 dark:bg-black"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Toast;
